90b60acc83e89729e789bd7c36b07a64c2963740
Reginaldo Alves de Barros <R21Regis@hotmail.com>
2018-01-31 22:09:37 -0200

    adicionei flag de autenticado

--- src/XF.AzureApp/XF.AzureApp/ViewModels/LoginViewModel.cs
+++ src/XF.AzureApp/XF.AzureApp/ViewModels/LoginViewModel.cs
@@ -12,13 +12,17 @@ namespace XF.AzureApp.ViewModels
 
         public string Senha { get; set; }
 
+        public bool Autenticado { get; set; }
+
         public UsuarioService UsuarioService => UsuarioService.Instance;
 
         public ICommand AutenticarCommand => new Command(async () => await Autenticar());
 
         public async Task Autenticar()
         {
-            if (await UsuarioService.Autorizado(Usuario, Senha))
+            Autenticado = await UsuarioService.Autorizado(Usuario, Senha);
+
+            if (Autenticado)
             {
                 await App.Current.MainPage.Navigation.PushAsync(new AtividadeView());
             }
